7aff1ef825fa05d98ccae42f55d3a8145e128db2
Aaron Gilman <gilmanaa2@gmail.com>
2019-06-07 16:33:18 +0300

    added level of difficutly feature

--- js/memory.js
+++ js/memory.js
@@ -9,12 +9,13 @@ function buildDeck() {
         }
     }
 }
+$("button:nth-child(2)").data("difficulty",6);
+$("button:nth-child(3)").data("difficulty",8);
+$("button:nth-child(4)").data("difficulty",10);
 $(".newGame").click(function () {
-    var easy = 6;
-    var medium = 9;
-    var difficult = 12;
-    newGame(easy);
-})
+    var difficulty = $(this).data("difficulty");
+    newGame(difficulty);
+});
 function newGame(difficulty) {
     buildDeck();
     $(".newGame").addClass("button_hide");
@@ -47,6 +48,9 @@ function newGame(difficulty) {
         boardPositions[k].append(placementImage[0]);
         boardPositions[k].append(coverImage[0]);
     }
+    $("img").each(function() {
+        $(this).addClass(`imgSize_${difficulty}`);
+    });
     var cardHide = $(".card_back");
     var cardShow = $(".card_front");
     var cardPosition1;
@@ -68,7 +72,7 @@ function newGame(difficulty) {
             cardPosition1 = $(this).data("name");
             cardClick.toggleClass("card_front");
             cardShowing1 = cardShow[cardPosition1];
-            cardShowing1.className = "card_back";
+            cardShowing1.className = `card_back imgSize_${difficulty}`;
             cardMatchData[0] = cardShowing1.src;
         } else if (clickCounter === 1) {
             clickCounter++;
@@ -76,16 +80,16 @@ function newGame(difficulty) {
             cardPosition2 = $(this).data("name");
             cardClick.toggleClass("card_front");
             cardShowing2 = cardShow[cardPosition2];
-            cardShowing2.className = "card_back";
+            cardShowing2.className = `card_back imgSize_${difficulty}`;
             cardMatchData[1] = cardShowing2.src;
             if (cardMatchData[0] !== cardMatchData[1]) {
                 function incorrectGuess() {
                     wrongGuesses++;
                     wrongGuessesContainer.text(`Wrong Guesses: ${wrongGuesses}`);
-                    cardShowing1.className = "card_front";
-                    cardShowing2.className = "card_front";
-                    cardHide[cardPosition1].className = "card_back";
-                    cardHide[cardPosition2].className = "card_back";
+                    cardShowing1.className = `card_front imgSize_${difficulty}`;
+                    cardShowing2.className = `card_front imgSize_${difficulty}`;
+                    cardHide[cardPosition1].className = `card_back imgSize_${difficulty}`;
+                    cardHide[cardPosition2].className = `card_back imgSize_${difficulty}`;
                     clickCounter = 0;
                 }
                 setTimeout(incorrectGuess, 1000);
@@ -99,21 +103,30 @@ function newGame(difficulty) {
             var container = $(".container");
             var youWon = $("<div>");
             var wrongGuessesFinal = $("<div>");
-            var playAgain = $("<button>");
+            var playAgainEasy = $("<button>");
+            var playAgainMedium = $("<button>");
+            var playAgainDifficult = $("<button>");
             youWon.addClass("winner");
             youWon.text("You Won!");
             youWon.appendTo(container);
-            playAgain.addClass("newGame");
-            playAgain.text("New Game");
-            playAgain.click(function () {
-                var easy = 6;
-                var medium = 9;
-                var difficult = 12;
-                newGame(easy);
-            })
+            playAgainEasy.addClass("newGame");
+            playAgainEasy.text("New Game Easy");
+            playAgainMedium.addClass("newGame");
+            playAgainMedium.text("New Game Medium");
+            playAgainDifficult.addClass("newGame");
+            playAgainDifficult.text("New Game Difficult");
             wrongGuessesFinal.text(`Wrong Guesses: ${wrongGuesses}`);
             wrongGuessesFinal.appendTo(youWon);
-            playAgain.appendTo(youWon);
+            playAgainEasy.appendTo(youWon);
+            playAgainMedium.appendTo(youWon);
+            playAgainDifficult.appendTo(youWon);
+            $("button:nth-child(2)").data("difficulty",6);
+            $("button:nth-child(3)").data("difficulty",8);
+            $("button:nth-child(4)").data("difficulty",10);
+            $(".newGame").click(function () {
+                var difficulty = $(this).data("difficulty");
+                newGame(difficulty);
+            });
         }
     })
 }
